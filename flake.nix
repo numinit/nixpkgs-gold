fed8f047e02d076888ad31e6b8beb7337f83b286
Tim Van Baak <tim.vanbaak@gmail.com>
2024-12-18 10:00:06 -0800

    Add flake support

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Developer SDK for nixpkgs gold(tm)";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          formatter = pkgs.nixfmt-rfc-style;
+        };
+      flake = {
+        lib = import ./lib.nix;
+      };
+    };
+}
