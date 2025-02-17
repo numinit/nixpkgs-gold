df14f3d0bf8b9575a87c5d3c0d960fb94587050f
Morgan Jones <me@numin.it>
2025-02-16 22:45:56 -0800

    flake: add check for noEula

--- flake.nix
+++ flake.nix
@@ -24,6 +24,14 @@
           ...
         }:
         {
+          # Useful overlay for nixpkgs that doesn't work for nixpkgs-gold because of the EULA.
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [
+              self.overlays.gold
+            ];
+          };
+
           # nixpkgs attribute automatically accepting the EULA. Only used for checks.
           _module.args.pkgsEulaAccepted = import inputs.nixpkgs {
             inherit system;
@@ -39,13 +47,30 @@
 
           checks =
             let
-              goldstd = drv: drv.override { stdenv = pkgsEulaAccepted.premenv; };
+              goldNoEula = attr: pkgs.${attr}.override { stdenv = pkgs.premenv; };
+              goldStd = attr: pkgsEulaAccepted.${attr}.override { stdenv = pkgsEulaAccepted.premenv; };
             in
             {
-              withoutLicense = (goldstd pkgsEulaAccepted.hello).overrideAttrs (prevAttrs: {
+              noEula = pkgs.stdenvNoCC.mkDerivation {
+                name = "no-eula";
+                inherit (builtins.tryEval (goldNoEula "hello")) success value;
+                phases = [ "checkPhase" "installPhase" ];
+                doCheck = true;
+                checkPhase = ''
+                  if [ -n "$success" ] || [ -n "$value" ]; then
+                    echo "Evaluating no EULA accepted derivation should have thrown." >&2
+                    echo "Instead we got ($success, $value)" >&2
+                    exit 1
+                  fi
+                '';
+                installPhase = ''
+                  ln -s /dev/null $out
+                '';
+              };
+              withoutLicense = (goldStd "hello").overrideAttrs (prevAttrs: {
                 name = "trial-${prevAttrs.pname}-${prevAttrs.version}";
               });
-              withLicense = (goldstd pkgsEulaAccepted.hello).overrideAttrs (prevAttrs: {
+              withLicense = (goldStd "hello").overrideAttrs (prevAttrs: {
                 name = "licensed-${prevAttrs.pname}-${prevAttrs.version}";
                 goldLicense = "X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*";
               });
